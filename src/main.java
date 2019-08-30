5d22c75df0605f8e2f30b5fc0b90ffd0e7731716
s4kis <gsakel5@gmail.com>
2019-08-30 13:52:11 +0300

    trying to find out the way i am going to make this work

--- src/main.java
+++ src/main.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class main {
+public class main
+{
+    public static void main(String[] args)
+    {
+        String arrEnglishLetters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "d", "y", "z"} ;
+        int counter = 0 ;
+        System.out.print("String total = {");
+        for (int i = 0 ; i < 26 ; i ++)
+        {
+            for (int j = 0 ; j < 26 ; j ++)
+            {
+                for (int k = 0 ; k < 26 ; k ++)
+                {
+                    counter ++ ;
+                    System.out.print('"' + arrEnglishLetters[i] + arrEnglishLetters[j] + arrEnglishLetters[k] + '"' + ", ");
+                }
+            }
+        }
+    }
 }
